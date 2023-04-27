1635cb8b171a1ce0cf52e6dee80e6ce1af8bfe21
spongebob <tejaspise931259@gmail.com>
2023-04-28 01:59:43 +0530

    april 28 2023 - 2am

---
+++ daily_leetcode.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    890. Find and Replace Pattern
+    https://leetcode.com/problems/find-and-replace-pattern/description/
+*/
+
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string> result;
+        for (string word : words) {
+            if (matchPattern(word, pattern)) {
+                result.push_back(word);
+            }
+        }
+        return result;
+    }
+    bool matchPattern(string word, string pattern) {
+        if (word.length() != pattern.length()) {
+            return false;
+        }
+        unordered_map<char, char> wordToPattern;
+        unordered_map<char, char> patternToWord;
+        for (int i = 0; i < word.length(); i++) {
+            char w = word[i];
+            char p = pattern[i];
+            if (wordToPattern.count(w) == 0 && patternToWord.count(p) == 0) {
+                wordToPattern[w] = p;
+                patternToWord[p] = w;
+            } else if (wordToPattern[w] != p || patternToWord[p] != w) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
