a9df018c88d99a29a3596438fd8f25787955466f
spongebob <tejaspise931259@gmail.com>
2023-05-01 23:56:19 +0530

    may 1 11:56pm

--- daily_leetcode.cpp
+++ daily_leetcode.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 /*
@@ -6,16 +6,16 @@ using namespace std;
     unordered_map<char, char> wordToPattern;
 
     ---------initialize all values to zero---------
-    int a[26] = {0}    
+    int a[26] = {0}
 
-    ---------find size--------- 
+    ---------find size---------
         string ==>   string.lenght()
         vector ==>   vector.size()
         array  ==>   int size = sizeof(arr)/ sizeof(arr[0])
-    
+
     ---------for each in cpp ---------
     for(string word : words2){
-    
+
     }
 
     --------- 2d vector with all values intiziles to zero ---------
@@ -27,58 +27,65 @@ using namespace std;
     --------- declare two variables in one line ---------
     int ext = p, ref = q;
 
-
+    --------- sorting an vector using func ---------
+    sort(vector.begin(), vector.end());
 
     --------- ---------
     --------- ---------
     --------- ---------
 */
-
-
 
 /*
     890. Find and Replace Pattern
     https://leetcode.com/problems/find-and-replace-pattern/description/
 */
 
-class Solution {
+class Solution
+{
 public:
-    bool domatch(string word, string pattern){
+    bool domatch(string word, string pattern)
+    {
 
-        if(word.length() != pattern.length()){
+        if (word.length() != pattern.length())
+        {
             return false;
         }
 
         unordered_map<char, char> wordToPattern;
         unordered_map<char, char> patternToWord;
 
-        for(int i =0; i< word.length(); i++ ){
+        for (int i = 0; i < word.length(); i++)
+        {
             char w = word[i];
             char p = pattern[i];
-            if(wordToPattern.count(w) == 0 && patternToWord.count(p) == 0 ){
+            if (wordToPattern.count(w) == 0 && patternToWord.count(p) == 0)
+            {
                 // mapping the letters if both are unmapped
                 wordToPattern[w] = p;
                 patternToWord[p] = w;
             }
-            else if(wordToPattern[w] != p || patternToWord[p] != w){
+            else if (wordToPattern[w] != p || patternToWord[p] != w)
+            {
                 // if they are not mapped to same letter
                 return false;
             }
         }
         return true;
     }
-    
-    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+
+    vector<string> findAndReplacePattern(vector<string> &words, string pattern)
+    {
         vector<string> result;
 
-        for(string word: words){                                        // same as foreach in other languages
-            if(domatch(word, pattern)){
+        for (string word : words)
+        { // same as foreach in other languages
+            if (domatch(word, pattern))
+            {
                 result.push_back(word);
             }
         }
         return result;
     }
-
 };
 
 /*
@@ -86,42 +93,51 @@ public:
     https://leetcode.com/problems/word-subsets/description/
 */
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+    vector<string> wordSubsets(vector<string> &words1, vector<string> &words2)
+    {
         vector<string> result;
         int ans[26] = {0};
-        for(string word : words2){
-            int sub[26] ={0};
+        for (string word : words2)
+        {
+            int sub[26] = {0};
             // --------- mapping word ---------
-            for(int i=0; i<word.length(); i++){        
+            for (int i = 0; i < word.length(); i++)
+            {
                 sub[word[i] - 'a']++;
             }
             // --------- checking the max as we only need to check the maximum occurance of a certain letter ---------
-            for(int i =0; i<26; i++){
+            for (int i = 0; i < 26; i++)
+            {
                 ans[i] = max(ans[i], sub[i]);
             }
         }
 
         // --------- checking if the pattern is same or not ---------
-        for(string word : words1){
+        for (string word : words1)
+        {
             int main[26] = {0};
-            for(int i=0; i<word.length(); i++){
+            for (int i = 0; i < word.length(); i++)
+            {
                 main[word[i] - 'a']++;
             }
             int i;
-            for(i=0; i<26; i++){
-                if(main[i] < ans[i]){
+            for (i = 0; i < 26; i++)
+            {
+                if (main[i] < ans[i])
+                {
                     break;
                 }
             }
-            if(i == 26){
+            if (i == 26)
+            {
                 result.push_back(word);
             }
         }
         return result;
     }
-
 };
 
 /*
@@ -130,19 +146,23 @@ public:
     --------- dynamic programming ---------
 
 */
-class Solution {
+class Solution
+{
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> path(m,vector<int>(n,1));
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> path(m, vector<int>(n, 1));
 
         // --------- we are counting all the possible ways to come to the current block ---------
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                path[i][j] = path[i-1][j] + path[i][j-1]; 
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                path[i][j] = path[i - 1][j] + path[i][j - 1];
             }
         }
         // --------- finally we get total number of possible ways to come to the last block ---------
-        return path[m-1][n-1];
+        return path[m - 1][n - 1];
     }
 };
 
@@ -150,31 +170,37 @@ public:
     378. Kth Smallest Element in a Sorted Matrix
     https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/description/
 */
-class Solution {
+class Solution
+{
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
+    int kthSmallest(vector<vector<int>> &matrix, int k)
+    {
         int low = matrix[0][0];
         int size = matrix.size();
-        int high = matrix[size-1][size-1];
+        int high = matrix[size - 1][size - 1];
 
-        while(low<high){
-            int mid = low + (high - low)/2;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
             int count = 0;
-            
+
             // --------- returns the number of small elements than mid element ---------
-            for(int i=0; i<size; i++){
+            for (int i = 0; i < size; i++)
+            {
                 count += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
             }
 
-            if(count < k){
+            if (count < k)
+            {
                 low = mid + 1;
             }
-            else{
+            else
+            {
                 high = mid;
             }
         }
         return low;
-    }   
+    }
 };
 
 /*
@@ -182,26 +208,82 @@ public:
     https://leetcode.com/problems/my-calendar-i/description/
 */
 
-class MyCalendar {
+class MyCalendar
+{
 public:
-    MyCalendar() {
-        
+    MyCalendar()
+    {
     }
-    map <int, int> m;
-    bool book(int start, int end) {
+    map<int, int> m;
+    bool book(int start, int end)
+    {
         m[start]++;
         m[end]--;
 
-        int sum =0;
-        for(auto elem: m){
+        int sum = 0;
+        for (auto elem : m)
+        {
             sum += elem.second;
-            if(sum>1){
+            if (sum > 1)
+            {
                 m[start]--;
                 m[end]++;
                 return false;
-            } 
+            }
         }
         return true;
+    }
+};
+
+/*
+    2. Add Two Numbers
+    https://leetcode.com/problems/add-two-numbers/description/
+*/
+
+//  Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {   
+        // ---------making new node and making it the start pointer  ---------
+        ListNode *ans = new ListNode();
+        // --------- new pointer for opreation ---------
+        ListNode *temp = ans;
+        int carry = 0;
+        while (l1 != NULL || l2 != NULL || carry)
+        {
+            int sum = 0;
+            if (l1 != NULL)
+            {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL)
+            {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            // --------- making new node and adding the digit value ---------
+            ListNode *newnode = new ListNode(sum % 10);
+            // --------- pointing next to the new node ---------
+            temp->next = newnode;
+            // ---------pointing new the pointer to the new note created ---------
+            temp = temp->next;
+        }
+        // --------- as we add every new node to next pointer so staring point is pointing to the first digit and it has no value stored ---------
+        return ans->next;
     }
 };
 
